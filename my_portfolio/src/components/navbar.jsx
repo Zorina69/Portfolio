8c7eff37d6e8a77dd811d1c8c8da54d1f566c76c
Zorina69 <soychanratana14@gmail.com>
2025-09-03 10:54:17 +0700

    commit

--- my_portfolio/src/components/navbar.jsx
+++ my_portfolio/src/components/navbar.jsx
@@ -5,9 +5,6 @@ const navbar = () => {
     <nav className='bg-gray-800 py-4 px-20 flex justify-between items-center'>
       <h1 className='text-white text-2xl'>My Portfolio</h1>
       <div className='flex space-x-4 gap-10'>
-        {/* <Link to="/aboutMe" className='text-gray-300 hover:text-white'>About Me</Link>
-        <Link to="/projects" className='text-gray-300 hover:text-white'>Projects</Link>
-        <Link to="/contact" className='text-gray-300 hover:text-white'>Contact</Link> */}
       </div>
     </nav>
   )
